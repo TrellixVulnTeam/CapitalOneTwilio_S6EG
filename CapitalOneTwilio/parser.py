87b95bc66271b1d754e0e0a37fbfeec01cce2624
Loren Heubert-Aubry <Loren Heubert-Aubry>
2017-03-25 16:00:58 -0400

    added preprocess function

--- CapitalOneTwilio/parser.py
+++ CapitalOneTwilio/parser.py
@@ -4,11 +4,16 @@ from textblob.classifiers import NaiveBayesClassifier
 with open('training.json', 'r') as fp:
     cl = NaiveBayesClassifier(fp, format="json")
 
-
+def preprocess(message):
+    message = message.lower()
+    return message
+    
 test = [("I want to talk to an agent.", 'call'),
         ("Transfer $65 from Checking to Savings", 'transfer'),
         ("Find ATMs near me", 'find'),
         ("What's my checking balance?", 'check')]
 
 for line in test:
+    line = preprocess(line)
     print(cl.classify(line[0]))
+
