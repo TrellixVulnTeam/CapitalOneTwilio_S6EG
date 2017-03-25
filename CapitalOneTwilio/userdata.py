cc9ea88d9ea6c4db0bb084278d3e6c617ed66dc4
nckalil <nckalil@umich.edu>
2017-03-25 14:29:29 -0400

    modify customer generator

--- CapitalOneTwilio/userdata.py
+++ CapitalOneTwilio/userdata.py
@@ -2,16 +2,15 @@
 # created by Noah Kalil - MHacks 9
 # 3/25/2017
 
-import urllib2
-import curl
+import urllib
 import json
 
 apiKey = '553d42f5192db2a972b9478ce912075a'
+url = 'http://api.reimaginebanking.com/customers?key=553d42f5192db2a972b9478ce912075a'
+url2 = 'https://mhacks9-7440d.firebaseio.com/Customers'
 
-for i in range(10)
-    url = 'http://api.reimaginebanking.com/customers?key=553d42f5192db2a972b9478ce912075a'
+for i in range(10):
     response = urllib2.urlopen(url)
 
     data = json.load(response)
-    curl -X POST -d 'data' \
-        'https://mhacks9-7440d.firebaseio.com/Customers.json'
+    request.post(url2,data)
